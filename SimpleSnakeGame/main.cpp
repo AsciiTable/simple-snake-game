608876f00758b0631822ee6450950b2d1f49135d
Jessica Wei <jessicawei9@gmail.com>
2020-05-24 10:54:44 -0700

    part 2

--- SimpleSnakeGame/main.cpp
+++ SimpleSnakeGame/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h> // all input output
 using namespace std;
 bool gameOver;
 const int width = 20;
@@ -31,6 +32,10 @@ void Draw() {
 				cout << "#";
 			else if (j == width - 1)
 				cout << "#";
+			else if (i == y && j == x)
+				cout << "O";
+			else if (i == fruitY && j == fruitX)
+				cout << "F";
 			else
 				cout << " ";
 		}
@@ -40,12 +45,60 @@ void Draw() {
 		cout << "#";
 	}
 	cout << endl;
+	cout << "Score: " << score << endl;
 }
 
 void Input() {
+	if (_kbhit()) { // if a keyboard key has been hit
+		switch (_getch()) {
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		default:
+			break;
+		}
+	}
 }
 
 void Logic() {
+	switch (dir) {
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	default:
+		break;
+	}
+
+	if (x > width || x < 0 || y > height || y < 0) {
+		gameOver = true;
+	}
+
+	if (x == fruitX && y == fruitY) {
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+	}
 }
 
 int main() {
