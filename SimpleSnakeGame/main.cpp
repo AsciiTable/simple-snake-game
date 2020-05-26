7fe02d11d6d7412c470cb6e547f43805f829d510
Jessica Wei <jessicawei9@gmail.com>
2020-05-25 21:57:09 -0700

    part 3

--- SimpleSnakeGame/main.cpp
+++ SimpleSnakeGame/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h> // all input output
+#include <Windows.h> // for Sleep
 using namespace std;
 bool gameOver;
 const int width = 20;
@@ -7,6 +8,8 @@ const int height = 20;
 int x, y; // head position
 int fruitX, fruitY; // fruit position
 int score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -36,8 +39,17 @@ void Draw() {
 				cout << "O";
 			else if (i == fruitY && j == fruitX)
 				cout << "F";
-			else
-				cout << " ";
+			else {
+				bool print = false;
+				for (int k = 0; k < nTail; k++) {
+					if (tailX[k] == j && tailY[k] == i) {
+						cout << "o";
+						print = true;
+					}
+				}
+				if(!print)
+					 cout << " ";
+			}
 		}
 		cout << endl;
 	}
@@ -70,9 +82,24 @@ void Input() {
 			break;
 		}
 	}
+	//else
+	//	dir = STOP;
 }
 
 void Logic() {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++) {
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
 	switch (dir) {
 	case LEFT:
 		x--;
@@ -90,11 +117,27 @@ void Logic() {
 		break;
 	}
 
-	if (x > width || x < 0 || y > height || y < 0) {
-		gameOver = true;
-	}
+	//if (x > width || x < 0 || y > height || y < 0) {
+	//	gameOver = true;
+	//}
 
+	if (x >= width)
+		x = 0;
+	else if (x < 0)
+		x = width - 1;
+
+	if (y >= height)
+		y = 0;
+	else if (y < 0)
+		y = height - 1;
+
+
+	for (int i = 0; i < nTail; i++) {
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
+	}
 	if (x == fruitX && y == fruitY) {
+		nTail++;
 		score += 10;
 		fruitX = rand() % width;
 		fruitY = rand() % height;
@@ -107,7 +150,7 @@ int main() {
 		Draw();
 		Input();
 		Logic();
-		// Sleep(10); // Slows our game a bit
+		Sleep(10); // Slows our game a bit
 	}
 	return 0;
 }
